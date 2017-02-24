14469260c5d9ed0052c790fddbc553b0e7b15931
keyboardr <keyboardr@gmail.com>
2017-02-23 22:46:54 -0800

    reenable debug

--- app/src/main/java/com/keyboardr/bluejay/player/PlaylistPlayer.java
+++ app/src/main/java/com/keyboardr/bluejay/player/PlaylistPlayer.java
@@ -22,7 +22,7 @@ import java.util.List;
 
 public class PlaylistPlayer extends AbsPlayer {
 
-  private static final boolean DEBUG_SHORT_SONGS = false;
+  private static final boolean DEBUG_SHORT_SONGS = true;
 
   public interface PlaylistChangedListener {
     void onQueueChanged();
