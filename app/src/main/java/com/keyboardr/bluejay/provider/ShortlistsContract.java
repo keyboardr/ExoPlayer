7e30d7b70b0c3a75ae99eb80600a81cb4808b93a
keyboardr <keyboardr@gmail.com>
2017-03-08 04:20:05 -0800

    Add position field to shortlists

--- app/src/main/java/com/keyboardr/bluejay/provider/ShortlistsContract.java
+++ app/src/main/java/com/keyboardr/bluejay/provider/ShortlistsContract.java
@@ -16,4 +16,7 @@ public interface ShortlistsContract extends ProviGenBaseContract {
 
     @Column(Column.Type.TEXT)
     String NAME = "name";
+
+    @Column(Column.Type.INTEGER)
+    String POSITION = "position";
 }
