e8c0104ba52e7ab2ff5e262ab81c52d368f803c0
keyboardr <keyboardr@gmail.com>
2017-05-05 22:14:30 -0700

    Add share button on tablets

--- app/src/main/java/com/keyboardr/bluejay/ui/monitor/library/HistoryFragment.java
+++ app/src/main/java/com/keyboardr/bluejay/ui/monitor/library/HistoryFragment.java
@@ -2,14 +2,18 @@ package com.keyboardr.bluejay.ui.monitor.library;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.LoaderManager;
+import android.support.v4.app.ShareCompat;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
+import android.support.v4.util.Pair;
 import android.support.v4.widget.SimpleCursorAdapter;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -18,12 +22,14 @@ import android.widget.ListView;
 import android.widget.PopupMenu;
 import android.widget.TextView;
 
+import com.jrummyapps.android.util.HtmlBuilder;
 import com.keyboardr.bluejay.R;
 import com.keyboardr.bluejay.bus.Buses;
 import com.keyboardr.bluejay.bus.event.SetMetadataEvent;
 import com.keyboardr.bluejay.model.FilterInfo;
 import com.keyboardr.bluejay.model.SetMetadata;
 import com.keyboardr.bluejay.provider.SetlistContract;
+import com.keyboardr.bluejay.provider.SetlistItemContract;
 import com.keyboardr.bluejay.ui.history.DeleteSetlistDialogFragment;
 import com.keyboardr.bluejay.ui.history.SetlistRenameDialogFragment;
 import com.keyboardr.bluejay.util.FragmentUtils;
@@ -75,14 +81,18 @@ public class HistoryFragment extends Fragment implements LoaderManager.LoaderCal
                 String name = ((TextView) newView.findViewById(android.R.id.text1)).getText()
                     .toString();
                 long id = setlists.getItemIdAtPosition(setlists.getPositionForView(newView));
-                if (item.getItemId() == R.id.rename) {
-                  SetlistRenameDialogFragment.newInstance(name, id)
-                      .show(getChildFragmentManager(), null);
-                  return true;
-                } else if (item.getItemId() == R.id.delete) {
-                  DeleteSetlistDialogFragment.newInstance(name, id)
-                      .show(getChildFragmentManager(), null);
-                  return true;
+                switch (item.getItemId()) {
+                  case R.id.rename:
+                    SetlistRenameDialogFragment.newInstance(name, id)
+                        .show(getChildFragmentManager(), null);
+                    return true;
+                  case R.id.share:
+                    shareSetlist(name, id);
+                    return true;
+                  case R.id.delete:
+                    DeleteSetlistDialogFragment.newInstance(name, id)
+                        .show(getChildFragmentManager(), null);
+                    return true;
                 }
                 return false;
               }
@@ -117,10 +127,46 @@ public class HistoryFragment extends Fragment implements LoaderManager.LoaderCal
     Buses.PLAYLIST.unregister(this);
   }
 
-  @NonNull
-  private String getSelectedSetlistName() {
-    return ((TextView) setlists.getSelectedView().findViewById(android.R.id.text1))
-        .getText().toString();
+  private void shareSetlist(final String name, final long id) {
+    new AsyncTask<Void, Void, Pair<String, String>>() {
+      @Override
+      protected Pair<String, String> doInBackground(Void... params) {
+        try (Cursor cursor = getContext().getContentResolver().query(
+            SetlistItemContract.CONTENT_URI,
+            new String[]{SetlistItemContract.ARTIST, SetlistItemContract.TITLE},
+            SetlistItemContract.SETLIST_ID + "=?",
+            new String[]{Long.toString(id)},
+            SetlistItemContract.POSITION)) {
+          StringBuilder plain = new StringBuilder(name).append('\n');
+          HtmlBuilder html = new HtmlBuilder().h4(name);
+          if (cursor != null && cursor.moveToFirst()) {
+            int artistColumn = cursor.getColumnIndexOrThrow(SetlistItemContract.ARTIST);
+            int titleColumn = cursor.getColumnIndexOrThrow(SetlistItemContract.TITLE);
+            html.open("ol");
+            do {
+              String song = cursor.getString(titleColumn) + " - " + cursor.getString(artistColumn);
+              plain.append(song).append('\n');
+              //noinspection NewApi
+              html.li(Html.escapeHtml(song));
+            } while (cursor.moveToNext());
+            plain.deleteCharAt(plain.length() - 1);
+            html.close();
+          }
+          return new Pair<>(plain.toString(), html.toString());
+        }
+      }
+
+      @Override
+      protected void onPostExecute(Pair<String, String> result) {
+        ShareCompat.IntentBuilder builder = ShareCompat.IntentBuilder.from(getActivity());
+        builder.setText(result.first);
+        builder.setHtmlText(result.second);
+        builder.setSubject(name);
+        builder.setChooserTitle(getString(R.string.share_formatted, name));
+        builder.setType("text/plain");
+        builder.startChooser();
+      }
+    }.execute();
   }
 
   @Subscribe(sticky = true, threadMode = ThreadMode.MAIN)
