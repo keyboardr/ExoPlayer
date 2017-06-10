f78e5dec323a646d1e0e1a0d107e233cf815855d
keyboardr <keyboardr@gmail.com>
2017-06-10 14:49:41 -0700

    Use CheckBox instead of CheckableTextView

--- app/src/main/java/com/keyboardr/bluejay/ui/shortlists/CheckableShortlistViewHolder.java
+++ app/src/main/java/com/keyboardr/bluejay/ui/shortlists/CheckableShortlistViewHolder.java
@@ -5,7 +5,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CheckedTextView;
+import android.widget.CheckBox;
 
 import com.keyboardr.bluejay.R;
 import com.keyboardr.bluejay.model.Shortlist;
@@ -16,11 +16,11 @@ import com.keyboardr.bluejay.model.Shortlist;
  */
 
 public abstract class CheckableShortlistViewHolder extends ShortlistViewHolder {
-  private CheckedTextView checkableView;
+  private CheckBox checkableView;
 
   public CheckableShortlistViewHolder(ViewGroup parent) {
     super(parent, R.layout.item_shortlist);
-    checkableView = ((CheckedTextView) textView);
+    checkableView = ((CheckBox) textView);
   }
 
   @Override
